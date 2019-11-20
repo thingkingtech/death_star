e4e9f106e92093b3b524605bdddb184d64b796ae
Thingking <39186510+thingkingtech@users.noreply.github.com>
2019-11-20 20:26:05 +0200

    Add files via upload

---
+++ leds_animate.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Common path for all GPIO access
+BASE_GPIO_PATH=/sys/class/gpio
+
+if [ ! -e $BASE_GPIO_PATH/gpio19 ] 
+	then
+		echo "19" > $BASE_GPIO_PATH/export
+		echo "out" > $BASE_GPIO_PATH/gpio19/direction
+		echo "0" > $BASE_GPIO_PATH/gpio19/value
+	else
+		echo "out" > $BASE_GPIO_PATH/gpio19/direction
+		echo "0" > $BASE_GPIO_PATH/gpio19/value
+fi
+
+if [ ! -e $BASE_GPIO_PATH/gpio25 ] 
+        then
+                echo "25" > $BASE_GPIO_PATH/export
+                echo "out" > $BASE_GPIO_PATH/gpio25/direction
+                echo "0" > $BASE_GPIO_PATH/gpio25/value
+        else
+                echo "out" > $BASE_GPIO_PATH/gpio25/direction
+                echo "0" > $BASE_GPIO_PATH/gpio25/value
+fi
+
+
+
+
---
+++ user_entry.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+TEXT=$1
+
+sudo killall screen
+sudo screen -dm /home/pi/rpi-rgb-led-matrix/examples-api-use/scrolling-text-example --led-rows=16 --led-chain=1 --led-scan-mode=1 --led-pwm-lsb-nanoseconds=1200 --led-slowdown-gpio=4 -f /home/pi/rpi-rgb-led-matrix/examples-api-use/fonts/9x18B.bdf $TEXT -C 255,0,0 -s 3 -y -2
